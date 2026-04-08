4a2200df8dafbd78993aa856c09ea7e314df988b
Anandshakya6398 <anandshakya6398@gmail.com>
2026-04-08 09:41:00 +0530

    connect with mysql_DB successfully

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const colors = require ('colors');
+const colors = require('colors');
 const app = express();
 const dotenv = require("dotenv");
-const morgan = require ("morgan");
+const morgan = require("morgan");
+const mySqlPool = require('./config/db');
 
 //config   dotenv
 dotenv.config();
@@ -12,15 +13,25 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 //routes
-app.get("/test", (req,res) => {
+app.get("/test", (req, res) => {
     res.status(200)
-    .send("<h1> Nodejs + SQL  sample</h1>");
+        .send("<h1> Nodejs + SQL  sample</h1>");
 });
 
 //port
 const PORT = process.env.PORT || 8080;
 
-//listen
-app.listen(PORT, () =>{
-    console.log("server Running.".bgMagenta.white);
-});
+//condtional listen
+mySqlPool.query('SELECT 1')
+    .then(() => {
+        //MYSQL
+        console.log('MYSQL DB CONNECTION'.bgCyan)
+
+        //listen
+        app.listen(PORT, () => {
+            console.log("server Running.".bgMagenta.white);
+        });
+
+    }).catch((error) => {
+        console.log(error);
+    });
